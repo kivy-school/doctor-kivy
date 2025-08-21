d45531336bc6233e0d52081723f29da9df1a8dbf
filipemarch <filipe.marchesini@gmail.com>
2025-08-21 16:05:37 -0300

    🐛 fix: Remove noexec from /work tmpfs to allow Python script execution

--- bot.py
+++ bot.py
@@ -126,7 +126,7 @@ class SimpleContainerPool:
                 "AutoRemove": True,
                 "Tmpfs": {
                     "/tmp": "size=80m,noexec,nosuid,nodev",  # 80MB /tmp
-                    "/work": "size=80m,noexec,nosuid,nodev",  # 80MB /work
+                    "/work": "size=80m,nosuid,nodev",  # 80MB /work
                 },
                 "Ulimits": [
                     {
